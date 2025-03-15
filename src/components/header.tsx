f0146bd74ea5d157b333a4a5e4e419c5304b8853
Dan G <daniel.w.gray1994@gmail.com>
2025-03-15 12:12:00 -0400

    changed default text color in app to black

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,7 +2,7 @@ import Logo from "./logo";
 
 export default function Header() {
   return (
-    <header className="bg-primary">
+    <header className="bg-primary text-white">
       <Logo />
     </header>
   );
