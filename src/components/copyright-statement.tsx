07b40bc584a52fb89124bdff8d1a56b12017231d
Dan G <daniel.w.gray1994@gmail.com>
2025-03-15 12:45:11 -0400

    added layout styling and copyright statement to footer

---
+++ src/components/copyright-statement.tsx
@@ -0,0 +1,3 @@
+export default function CopyrightStatement() {
+  return <p className="text-sm">QuizMo &copy;2025 All rights reserved.</p>;
+}
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,3 +1,9 @@
+import CopyrightStatement from "./copyright-statement";
+
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer className="bg-primary/15 text-black/45 flex flex-col items-center py-4">
+      <CopyrightStatement />
+    </footer>
+  );
 }
