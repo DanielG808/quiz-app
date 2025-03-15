dc23b3a2d5fa93c91bbe098d573a28d9ca25e22e
Dan G <daniel.w.gray1994@gmail.com>
2025-03-15 16:18:41 -0400

    added label to radio buttons and layout styling to buttons and label

--- src/components/type-selector.tsx
+++ src/components/type-selector.tsx
@@ -2,9 +2,12 @@ import RadioButton from "./ui/radio-button";
 
 export default function TypeSelector() {
   return (
-    <div className="pl-4 py-3">
-      <RadioButton />
-      <RadioButton />
+    <div className="py-3 flex flex-col">
+      <label htmlFor="">Quiz Type:</label>
+      <div className="flex items-center pt-2">
+        <RadioButton type="Flashcard" />
+        <RadioButton type="Multiple Choice" />
+      </div>
     </div>
   );
 }
--- src/components/ui/radio-button.tsx
+++ src/components/ui/radio-button.tsx
@@ -1,9 +1,13 @@
-export default function RadioButton() {
+type RadioButtonProps = {
+  type: "Flashcard" | "Multiple Choice";
+};
+
+export default function RadioButton({ type }: RadioButtonProps) {
   return (
     <label className="inline-flex items-center space-x-2 cursor-pointer px-2">
       <input type="radio" className="hidden peer" />
       <span className="w-5 h-5 border-2 border-gray-300 rounded-full peer-checked:border-primary peer-checked:bg-primary/45 transition-all duration-200" />
-      <span>Type</span>
+      <span className="text-sm">{type}</span>
     </label>
   );
 }
