f29bf7620df50fc23bba221ae72b3df99453e624
Dan G <daniel.w.gray1994@gmail.com>
2025-03-15 14:07:56 -0400

    added layout styling to close button and header in dialog

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,15 +1,19 @@
-import React from "react";
+import { cn } from "@/lib/utils/cn";
 
 type ButtonProps = {
   onClick: () => void;
   children: React.ReactNode;
+  className?: string;
 };
 
-export default function Button({ onClick, children }: ButtonProps) {
+export default function Button({ onClick, children, className }: ButtonProps) {
   return (
     <button
       onClick={onClick}
-      className="bg-primary/45 text-background rounded-md px-4 py-2 hover:cursor-pointer hover:bg-primary/85 hover:text-white transition-all duration-200"
+      className={cn(
+        "bg-primary/45 text-background rounded-md px-4 py-2 hover:cursor-pointer hover:bg-primary/85 hover:text-white transition-all duration-200",
+        className
+      )}
     >
       {children}
     </button>
--- src/components/ui/dialog-box.tsx
+++ src/components/ui/dialog-box.tsx
@@ -1,5 +1,7 @@
 import H1 from "../H1";
 import Button from "./button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faX } from "@fortawesome/free-solid-svg-icons";
 
 type DialogBoxProps = {
   closeDialog: () => void;
@@ -7,10 +9,15 @@ type DialogBoxProps = {
 
 export default function DialogBox({ closeDialog }: DialogBoxProps) {
   return (
-    <section className="fixed inset-0 flex justify-center items-center bg-black/65 z-50">
-      <div className="bg-white w-2/3 rounded-md p-4">
-        <div className="">
-          <Button onClick={closeDialog}>X</Button>
+    <section className="fixed inset-0 flex justify-center items-center bg-black/55 z-50">
+      <div className="bg-white w-1/3 rounded-md p-4">
+        <div className="flex justify-end items-center">
+          <Button
+            onClick={closeDialog}
+            className="text-md bg-white text-black/65 p-0 hover:text-black hover:bg-white"
+          >
+            <FontAwesomeIcon icon={faX} />
+          </Button>
         </div>
         <H1 className="text-2xl">Create a New Quiz:</H1>
       </div>
