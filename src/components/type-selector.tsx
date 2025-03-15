fb2be89c84eb6cc2b7e387f23a50b1314b77ee10
Dan G <daniel.w.gray1994@gmail.com>
2025-03-15 16:07:49 -0400

    added layout styling to the radio-button

--- src/components/type-selector.tsx
+++ src/components/type-selector.tsx
@@ -2,7 +2,7 @@ import RadioButton from "./ui/radio-button";
 
 export default function TypeSelector() {
   return (
-    <div>
+    <div className="pl-4 py-3">
       <RadioButton />
       <RadioButton />
     </div>
--- src/components/ui/radio-button.tsx
+++ src/components/ui/radio-button.tsx
@@ -1,6 +1,6 @@
 export default function RadioButton() {
   return (
-    <label className="inline-flex items-center space-x-2 cursor-pointer">
+    <label className="inline-flex items-center space-x-2 cursor-pointer px-2">
       <input type="radio" className="hidden peer" />
       <span className="w-5 h-5 border-2 border-gray-300 rounded-full peer-checked:border-primary peer-checked:bg-primary/45 transition-all duration-200" />
       <span>Type</span>
