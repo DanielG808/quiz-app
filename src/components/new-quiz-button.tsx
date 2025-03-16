739fae21366f06135768788e01cbfc51f0b7b002
Dan G <daniel.w.gray1994@gmail.com>
2025-03-16 09:24:09 -0400

    refactored dialog-box state into custom hook useDialogBox

--- src/components/new-quiz-button.tsx
+++ src/components/new-quiz-button.tsx
@@ -1,14 +1,11 @@
 "use client";
 
-import { useState } from "react";
 import Button from "./ui/button";
 import DialogBox from "./ui/dialog-box";
+import useDialogBox from "@/lib/hooks/useDialogBox";
 
 export default function NewQuizButton() {
-  const [isDialogOpen, setIsDialogOpen] = useState(false);
-
-  const openDialog = () => setIsDialogOpen(true);
-  const closeDialog = () => setIsDialogOpen(false);
+  const { isDialogOpen, openDialog, closeDialog } = useDialogBox();
 
   return (
     <div className="w-full flex flex-col items-end pt-4 pr-4">
