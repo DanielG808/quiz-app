0c0d5129dad9c44afba80eade08644d8d8b3c756
Dan G <daniel.w.gray1994@gmail.com>
2025-03-16 09:52:20 -0400

    created useOnClickOutside custom hook and applied it to dialog-box

--- src/components/ui/dialog-box.tsx
+++ src/components/ui/dialog-box.tsx
@@ -1,6 +1,8 @@
 import Button from "./button";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faX } from "@fortawesome/free-solid-svg-icons";
+import { useRef } from "react";
+import useOnClickOutside from "@/lib/hooks/useOnClickOutside";
 
 type DialogBoxProps = {
   closeDialog: () => void;
@@ -8,9 +10,13 @@ type DialogBoxProps = {
 };
 
 export default function DialogBox({ closeDialog, children }: DialogBoxProps) {
+  const dialogRef = useRef<HTMLDivElement | null>(null);
+
+  useOnClickOutside(dialogRef, closeDialog);
+
   return (
     <section className="fixed inset-0 flex justify-center items-center bg-black/55 z-50">
-      <div className="bg-white w-1/3 rounded-md p-4">
+      <div ref={dialogRef} className="bg-white w-1/3 rounded-md p-4">
         <div className="flex justify-end items-center">
           <Button
             onClick={closeDialog}
