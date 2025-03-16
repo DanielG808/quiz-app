572364a9b00315f2d8dc544bba5a453ebc365c42
Dan G <daniel.w.gray1994@gmail.com>
2025-03-16 11:28:15 -0400

    tested that hook stops listening when ref element unmounts for useOnClickOutside

--- src/lib/hooks/useOnClickOutside.test.ts
+++ src/lib/hooks/useOnClickOutside.test.ts
@@ -86,4 +86,41 @@ describe("useOnClickOutside Hook", () => {
 
     document.body.removeChild(div);
   });
+
+  test("should call the handler when clicking outside the ref element", () => {
+    const div = document.createElement("div");
+    document.body.appendChild(div);
+
+    ref.current = div;
+
+    const { unmount } = renderHook(() => useOnClickOutside(ref, handler));
+
+    act(() => {
+      const outsideEvent = new MouseEvent("mousedown", {
+        bubbles: true,
+        cancelable: true,
+        clientX: 0,
+        clientY: 0,
+      });
+      document.body.dispatchEvent(outsideEvent);
+    });
+
+    expect(handler).toHaveBeenCalledTimes(1);
+
+    unmount();
+
+    act(() => {
+      const outsideEvent = new MouseEvent("mousedown", {
+        bubbles: true,
+        cancelable: true,
+        clientX: 0,
+        clientY: 0,
+      });
+      document.body.dispatchEvent(outsideEvent);
+    });
+
+    expect(handler).toHaveBeenCalledTimes(1);
+
+    document.body.removeChild(div);
+  });
 });
